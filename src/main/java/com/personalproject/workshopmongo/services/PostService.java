0768c17bc607613a4f3a2a2dbb398e4ae447465f
francisco-wagner <franciscowagner200@gmail.com>
2024-06-18 14:21:07 -0300

    Consulta com vários critérios

--- src/main/java/com/personalproject/workshopmongo/services/PostService.java
+++ src/main/java/com/personalproject/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.personalproject.workshopmongo.services.exeptions.ObjectNotFoundExcept
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repository.searchTitle(text);
+    }
+
+    public List<Post> fullsearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() +24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
